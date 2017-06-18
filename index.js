ccae9341c6d0b759707df7f7974f408573fe441d
xcfm <365326561@qq.com>
2017-06-18 19:38:50 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+const sockets = new Map();
+var socketsid = 0;
+
+wss.on('connection', ws => {
+
+    ws.on('message', message => {
+        console.log('received: %s', message);
+        if (message == 'reg') {
+            ws.extensions = socketsid++;
+            ws.send('reg' + ws.extensions);
+            console.log('send:' + 'reg' + ws.extensions)
+        }
+        if (message.indexOf('login') != -1) {
+            var loginWS = message.split('-');
+            var login = parseInt(loginWS[1]);
+            var loginSuccess = false;
+            wss.clients.forEach(ws2 => {
+                if (ws2.extensions == login) {
+                    ws.send('loginSuccess')
+                    ws2.send('loginSuccess')
+                    loginSuccess = true;
+                }
+            })
+            if (!loginSuccess) {
+                ws.send('二维码已过期')
+                console.log('send:' + '二维码已过期')
+            }
+
+        }
+    });
+
+});
